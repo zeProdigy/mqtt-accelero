69d4db974efb68a7e34d7cecfdc26c3c387f7ab3
zeProdigy <you@example.com>
2021-11-23 18:10:46 +0300

    sys::spi: accelero spi conf wip

--- system/spi_list.c
+++ system/spi_list.c
@@ -1,7 +1,29 @@
 #include <system/spi.h>
 
 spi_conf_t spi_list[] = {
+    // ACCELERO_SPI
+    {
+        .handler = {
+            .Instance = SPI1,
+            .Init = {
+                .Mode = SPI_MODE_MASTER,
+                .CLKPolarity = SPI_POLARITY_LOW,
+                .CLKPhase = SPI_PHASE_1EDGE,
+                .NSS = SPI_NSS_SOFT,
+                .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16,
+            }
+        },
+        .irq = {
 
+        },
+        .rcc = {
+
+        },
+        .cs_pin = ,
+        .miso_pin = ,
+        .mosi_pin = ,
+        .sclk_pin = ,
+    }
 };
 
 const size_t spi_list_len = (sizeof(spi_list) / sizeof(spi_conf_t));
